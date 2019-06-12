301f8e3dc79865e1985f2ed2ae7440d50fbb0693
whwayne <whwayne@gmail.com>
2019-06-12 15:18:10 -0500

    third commit

--- Lab_8_HomeWork/day2/prob3/Main.java
+++ Lab_8_HomeWork/day2/prob3/Main.java
@@ -1,10 +1,27 @@
 package Lab_8_HomeWork.day2.prob3;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Main {
 
     public static void main(String[] args) {
         List<String> fruits = Arrays.asList("Apple", "Banana", "Orange", "Cherries", "blums");
+
+        Consumer<String> consumer = new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println(s);
+            }
+        };
+
+        fruits.forEach(consumer);
+
+        for (String s : fruits) {
+            System.out::println(s);
+        }
+
+
+
     }
 }
